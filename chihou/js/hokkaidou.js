9dedb70801ebbcb74983e4fda080e7d1d046fdfb
nao11413 <naokisumida0208@gmail.com>
2022-06-13 02:09:33 +0900

    あ

--- chihou/js/hokkaidou.js
+++ chihou/js/hokkaidou.js
@@ -1,8 +1,8 @@
 var weatherHokkaidou = {
-  const request = new XMLHttpRequest();
-     id = 011000;
-     URL = 'https:weather.tsukumijima.net/api/forecast/city=id&dateLabel = "明日"&chanceOfRain=T12_18';
-     request.open('GET', URL, true);
+  const request = new XMLHttpRequest(),
+     id = 011000,
+     URL = 'https:weather.tsukumijima.net/api/forecast/city=id&dateLabel = "明日"&chanceOfRain=T12_18',
+     request.open('GET', URL, true),
       console.log(URL);
 /*  display: function(syuu[]) {  // 匿名関数でメソッド定義
     syuu.area.forEach(function( id ,spot) {
