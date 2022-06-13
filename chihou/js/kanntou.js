ee46291d6fc711e711864d111855748fe53f18f3
take <shirota.goo@gmail.com>
2022-06-14 03:30:13 +0900

    chnage min

--- chihou/js/kanntou.js
+++ chihou/js/kanntou.js
@@ -16,9 +16,9 @@ request.send();
  }
 
 display('https:weather.tsukumijima.net/api/forecast?city=080010','ibaragi'); //茨城
-display('https:weather.tsukumijima.net/api/forecast?city=090010','totigi'); //栃木
+display('https:weather.tsukumijima.net/api/forecast?city=090010','tochigi'); //栃木
 display('https:weather.tsukumijima.net/api/forecast?city=100010','gunnma'); //群馬
 display('https:weather.tsukumijima.net/api/forecast?city=110010','saitama'); //埼玉
-display('https:weather.tsukumijima.net/api/forecast?city=120010','tiba'); //千葉
+display('https:weather.tsukumijima.net/api/forecast?city=120010','chiba'); //千葉
 display('https:weather.tsukumijima.net/api/forecast?city=130010','tokyo'); //東京
 display('https:weather.tsukumijima.net/api/forecast?city=140010','kanagawa'); //神奈川
