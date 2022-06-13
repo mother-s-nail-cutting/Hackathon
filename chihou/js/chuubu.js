43587b22898726d577aaed39a7fe2a8b83b5427c
take <shirota.goo@gmail.com>
2022-06-13 22:10:32 +0900

    change

--- chihou/js/chuubu.js
+++ chihou/js/chuubu.js
@@ -1,6 +1,6 @@
 fetch('https:weather.tsukumijima.net/api/forecast?city=400040')
   .then(response =>response.json())
-  .then(json => console.log(json.description.text))
+  .then(json => console.log(json.forecasts[0].chanceOfRain.T18_24))
 
 
 
