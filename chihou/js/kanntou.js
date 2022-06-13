961dd553eb1aa99787d6b8c06d263d5decdffd9d
nao11413 <naokisumida0208@gmail.com>
2022-06-14 02:58:47 +0900

    tyuubu

--- chihou/js/kanntou.js
+++ chihou/js/kanntou.js
@@ -1,4 +1,4 @@
-function display(URL) {
+function display(URL,place) {
    var request = new XMLHttpRequest();
      request.open('GET', URL, true);
      request.responseType = 'json'
@@ -6,15 +6,19 @@ function display(URL) {
        const date = this.response.forecasts[0].chanceOfRain.T18_24;
        if(date == "0%"||date == "10%"||date == "20%"){
        console.log(date);
-       i++,
        console.log("晴れ：遊び場を全て表示する");
      }
-     else console.log("雨：降水確率20%以下のみ表示する");
+     else {document.getElementById(place).classList.add('rain');
+       console.log("雨：降水確率2１%以上");
+     }
    }
 request.send();
  }
 
-display('https:weather.tsukumijima.net/api/forecast?city=011000'); //道北
-display('https:weather.tsukumijima.net/api/forecast?city=013010'); //道東
-display('https:weather.tsukumijima.net/api/forecast?city=015010'); //道南
-display('https:weather.tsukumijima.net/api/forecast?city=016010'); //道央
+display('https:weather.tsukumijima.net/api/forecast?city=080010','ibaragi'); //茨城
+display('https:weather.tsukumijima.net/api/forecast?city=090010','totigi'); //栃木
+display('https:weather.tsukumijima.net/api/forecast?city=100010','gunnma'); //群馬
+display('https:weather.tsukumijima.net/api/forecast?city=110010','saitama'); //埼玉
+display('https:weather.tsukumijima.net/api/forecast?city=120010','tiba'); //千葉
+display('https:weather.tsukumijima.net/api/forecast?city=130010','tokyo'); //東京
+display('https:weather.tsukumijima.net/api/forecast?city=140010','kanagawa'); //神奈川
