a34628cea306ddb66884430515a51226bd9e944f
nao11413 <naokisumida0208@gmail.com>
2022-06-14 16:49:36 +0900

    a

--- chihou/js/chuubu.js
+++ chihou/js/chuubu.js
@@ -1,4 +1,4 @@
-function display(URL) {
+function display(URL,place) {
    var request = new XMLHttpRequest();
      request.open('GET', URL, true);
      request.responseType = 'json'
@@ -6,10 +6,11 @@ function display(URL) {
        const date = this.response.forecasts[0].chanceOfRain.T18_24;
        if(date == "0%"||date == "10%"||date == "20%"){
        console.log(date);
-       i++,
        console.log("晴れ：遊び場を全て表示する");
      }
-     else console.log("雨：降水確率20%以下のみ表示する");
+     else {document.getElementById(place).classList.add('rain');
+       console.log("雨：降水確率2１%以上");
+     }
    }
 request.send();
  }
@@ -18,7 +19,7 @@ request.send();
  display('https:weather.tsukumijima.net/api/forecast?city=160010','toyama'); //富山
  display('https:weather.tsukumijima.net/api/forecast?city=170010','ishikawa'); //石川
  display('https:weather.tsukumijima.net/api/forecast?city=180010','hukui'); //福井
- display('https:weather.tsukumijima.net/api/forecast?city=190010','yamanasi'); //山梨
+ display('https:weather.tsukumijima.net/api/forecast?city=190010','yamanashi'); //山梨
  display('https:weather.tsukumijima.net/api/forecast?city=200010','nagano'); //長野
  display('https:weather.tsukumijima.net/api/forecast?city=210010','gihu'); //岐阜
  display('https:weather.tsukumijima.net/api/forecast?city=220010','shizuoka'); //静岡
