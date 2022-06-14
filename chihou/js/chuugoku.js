2b9420c969af0c0198481df7115660a4daabb28d
nao11413 <naokisumida0208@gmail.com>
2022-06-14 17:55:50 +0900

    js変更

--- chihou/js/chuugoku.js
+++ chihou/js/chuugoku.js
@@ -6,14 +6,15 @@ function display(URL,place) {
        const date = this.response.forecasts[1].chanceOfRain.T12_18;
        if(date == "0%"||date == "10%"||date == "20%"){
        console.log(date);
-       console.log("晴れ：遊び場を全て表示する");
+       console.log("晴れ："+ date);
      }
      else {document.getElementById(place).classList.add('rain');
-       console.log("雨：降水確率2１%以上");
+       console.log("雨が降るかも：" + date);
      }
    }
 request.send();
  }
+
 
  display('https:weather.tsukumijima.net/api/forecast?city=310010','tottori'); //鳥取
  display('https:weather.tsukumijima.net/api/forecast?city=320010','shimane'); //島根
