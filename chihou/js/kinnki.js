98f717234123ce9f0b21ca7decff8c6bf7377626
nao11413 <naokisumida0208@gmail.com>
2022-06-14 17:11:16 +0900

    a

--- chihou/js/kinnki.js
+++ chihou/js/kinnki.js
@@ -17,10 +17,8 @@ request.send();
 
  display('https:weather.tsukumijima.net/api/forecast?city=240010','mie'); //三重
  display('https:weather.tsukumijima.net/api/forecast?city=250010','shiga'); //滋賀
- display('https:weather.tsukumijima.net/api/forecast?city=260010','kyouto'); //京都
+ display('https:weather.tsukumijima.net/api/forecast?city=260010','kyoto'); //京都
  display('https:weather.tsukumijima.net/api/forecast?city=270010','osaka'); //大阪
  display('https:weather.tsukumijima.net/api/forecast?city=280010','hyougo'); //兵庫
  display('https:weather.tsukumijima.net/api/forecast?city=290010','nara'); //奈良
  display('https:weather.tsukumijima.net/api/forecast?city=300010','wakayama'); //和歌山
- display('https:weather.tsukumijima.net/api/forecast?city=310010','shizuoka'); //静岡
- display('https:weather.tsukumijima.net/api/forecast?city=320010','aiti'); //愛知
